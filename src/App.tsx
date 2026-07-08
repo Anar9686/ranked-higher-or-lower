0973926d24dc315fc607b0b4efcd716bb99fc15a
Anar9686 <anarzit333@gmail.com>
2026-07-09 00:06:19 +0900

    Logo on header, Landing page
    Busy day coudln't do much
    TODO:
    Put score somewhere iwth high score
    Animate so revealed pane goes to left and reveal new pane.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import MainPage from "./pages/MainPage";
 import BuyMeCoffee from "./components/BuyMeCoffee";
 import Header from "./components/Header";
+import { AnimatePresence } from "motion/react";
+import LandingDialog from "./components/LandingDialog";
 
 export default function App() {
+  const [showLanding, setShowLanding] = useState(() => {
+    const seen = sessionStorage.getItem("hol-seen");
+    if (!seen) {
+      sessionStorage.setItem("hol-seen", "1");
+      return true;
+    }
+    return false;
+  });
 
   return (
     <div className="h-screen w-screen overflow-hidden flex flex-col bg-[#080808]">
@@ -11,6 +22,10 @@ export default function App() {
       <main className="flex-1 flex relative overflow-hidden">
         <MainPage/>
       </main>
+
+      <AnimatePresence>
+        {showLanding && <LandingDialog onClose={() => setShowLanding(false)} />}
+      </AnimatePresence>
 
       <BuyMeCoffee />
     </div>
